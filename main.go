a000877a3669f0fb8dc849dd8b29a5570facad26
zeroshirts <zeroshirts@users.noreply.github.com>
2015-08-19 19:35:34 -0500

    tweaks

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Quick and dirty DNS resolver with SOCKS5 proxy support
 package main
 
 import (
@@ -7,6 +8,7 @@ import (
 	"github.com/miekg/dns"
 	"golang.org/x/net/proxy"
 	"log"
+	"net"
 	"runtime"
 	"time"
 )
@@ -16,19 +18,15 @@ import (
 Other miekg/dns implementations:
 - https://github.com/d2g/dnsforwarder
 - https://github.com/googollee/dnsproxy/blob/master/client.go
+- https://github.com/DJDNS/djdns
 
 TODO: print some stats every 5min?
-
 */
 
 var (
-	address      = flag.String("address", "127.0.0.1:53", "Address to listen to (TCP and UDP)")
-	socks5Proxy  = flag.String("socks5", "", "SOCKS5 address and port")
-	proxySideDNS = flag.String("proxydns", "8.8.8.8:53", "Proxy-side DNS server")
-)
-
-var (
-	_proxyChan chan proxyRequest
+	address     = flag.String("address", "127.0.0.1:53", "Address to listen to (TCP and UDP)")
+	socks5Proxy = flag.String("socks5", "", "SOCKS5 address and port")
+	dnsServer   = flag.String("dns", "8.8.8.8:53", "DNS server")
 )
 
 func isTransfer(req *dns.Msg) bool {
@@ -47,99 +45,108 @@ type proxyResponse struct {
 }
 
 type proxyRequest struct {
-	req          *dns.Msg
-	responseChan chan proxyResponse
+	*dns.Msg
+	response chan proxyResponse
 }
 
-func proxyWorkerFunc(done chan proxyResponse, req *dns.Msg) {
-	dialer, err := proxy.SOCKS5("tcp", "127.0.0.1:9123", nil, proxy.Direct)
-	if err != nil {
-		done <- proxyResponse{nil, err}
-		return
+func dial() (net.Conn, error) {
+	if *socks5Proxy == "" {
+		return proxy.Direct.Dial("tcp", *dnsServer)
 	}
-	proxyCon, err := dialer.Dial("tcp", *proxySideDNS)
+	dialer, err := proxy.SOCKS5("tcp", *socks5Proxy, nil, proxy.Direct)
 	if err != nil {
-		done <- proxyResponse{nil, err}
-		return
+		return nil, err
 	}
-	defer func() {
-		if err = proxyCon.Close(); err != nil {
-			panic(err)
+	return dialer.Dial("tcp", *dnsServer)
+}
+
+func proxyWorkerFunc2(req *dns.Msg) (*dns.Msg, error) {
+	// timer expiration (+1) and DNS write/read completes (+1) == 2
+	done := make(chan proxyResponse, 2)
+
+	go func() {
+		// dial() can block (think: getsockopt)
+		conn, err := dial()
+		if err != nil {
+			done <- proxyResponse{nil, err}
+			return
 		}
+
+		defer func() {
+			if err := conn.Close(); err != nil {
+				log.Printf("conn.Close() error: %v", err.Error())
+			}
+		}()
+
+		if isTransfer(req) {
+			err := errors.New("need to handle transfers!")
+			done <- proxyResponse{nil, err}
+			return
+
+			/*
+				// from: https://github.com/StalkR/dns-reverse-proxy
+				if transport != "tcp" {
+					log.Printf("ERROR: isTransfer==true and transport==", transport)
+					dns.HandleFailed(w, req)
+					return
+				}
+				t := new(dns.Transfer)
+				c, err := t.In(req, addr)
+				if err != nil {
+					log.Print("ERROR: " + err.Error())
+					dns.HandleFailed(w, req)
+					return
+				}
+				if err = t.Out(w, req, c); err != nil {
+					log.Print("ERROR: " + err.Error())
+					dns.HandleFailed(w, req)
+					return
+				}
+				return
+			*/
+		}
+
+		dnsConn := &dns.Conn{Conn: conn}
+		if err := dnsConn.WriteMsg(req); err != nil {
+			done <- proxyResponse{nil, err}
+			return
+		}
+
+		resp, err := dnsConn.ReadMsg()
+		if err != nil {
+			done <- proxyResponse{nil, err}
+			return
+
+		} else if resp.Id != req.Id {
+			err := fmt.Errorf("ERROR: resp.Id %v != req.Id %v", resp.Id, req.Id)
+			done <- proxyResponse{nil, err}
+			return
+		}
+		/*
+			// is this necessary? what effect does it have?
+			resp.RecursionAvailable = true
+		*/
+		done <- proxyResponse{resp, nil}
 	}()
 
-	if isTransfer(req) {
-		err := errors.New("isTransfer==true! handle this!")
-		done <- proxyResponse{nil, err}
-		return
+	select {
+	case <-time.After(10 * time.Second):
+		err := errors.New("general timeout")
+		return nil, err
 
-		/*
-			// from: https://github.com/StalkR/dns-reverse-proxy
-			if transport != "tcp" {
-				log.Printf("ERROR: isTransfer==true and transport==", transport)
-				dns.HandleFailed(w, req)
-				return
-			}
-			t := new(dns.Transfer)
-			c, err := t.In(req, addr)
-			if err != nil {
-				log.Print("ERROR: " + err.Error())
-				dns.HandleFailed(w, req)
-				return
-			}
-			if err = t.Out(w, req, c); err != nil {
-				log.Print("ERROR: " + err.Error())
-				dns.HandleFailed(w, req)
-				return
-			}
-			return
-		*/
+	case r := <-done:
+		return r.Msg, r.err
 	}
-
-	co := &dns.Conn{Conn: proxyCon}
-	if err := co.WriteMsg(req); err != nil {
-		done <- proxyResponse{nil, err}
-		return
-	}
-	resp, err := co.ReadMsg()
-	if err != nil {
-		done <- proxyResponse{nil, err}
-		return
-
-	} else if resp.Id != req.Id {
-		err := fmt.Errorf("ERROR: resp.Id %v != req.Id %v", resp.Id, req.Id)
-		done <- proxyResponse{nil, err}
-		return
-	}
-	/*
-		// is this necessary? what effect does it have?
-		resp.RecursionAvailable = true
-	*/
-	done <- proxyResponse{resp, nil}
 }
 
 func proxyWorker(c chan proxyRequest) {
 	for req := range c {
-		/*
-			edge-case: it's possible two responses can be sent IF our timer expires,
-			and the DNS exchange actually finishes after..  we only need to relay
-			the first, the second will be GC'd
-		*/
-		done := make(chan proxyResponse, 2)
-		go proxyWorkerFunc(done, req.req)
-
-		select {
-		case <-time.After(10 * time.Second):
-			err := errors.New("some kind of timeout!")
-			req.responseChan <- proxyResponse{nil, err}
-		case r := <-done:
-			req.responseChan <- r
-		}
+		resp, err := proxyWorkerFunc2(req.Msg)
+		req.response <- proxyResponse{resp, err}
 	}
 }
 
-// TODO: Consider singleflight!
-func route(w dns.ResponseWriter, req *dns.Msg) {
+func route(w dns.ResponseWriter, req *dns.Msg, jobQueue chan proxyRequest) {
 	if len(req.Question) == 0 {
 		log.Print("ERROR: len(req.Question)==0")
 		dns.HandleFailed(w, req)
@@ -147,7 +154,7 @@ func route(w dns.ResponseWriter, req *dns.Msg) {
 	}
 
 	responseChan := make(chan proxyResponse, 0)
-	_proxyChan <- proxyRequest{req, responseChan}
+	jobQueue <- proxyRequest{req, responseChan}
 	x := <-responseChan
 	close(responseChan)
 
@@ -157,6 +164,7 @@ func route(w dns.ResponseWriter, req *dns.Msg) {
 		return
 	}
 
+	// assuming miekg/dns handles possible indefinite write blocking
 	if err := w.WriteMsg(x.Msg); err != nil {
 		log.Print("ERROR:" + err.Error())
 		dns.HandleFailed(w, req)
@@ -165,19 +173,30 @@ func route(w dns.ResponseWriter, req *dns.Msg) {
 	////log.Print(x.Msg.String())
 }
 
+// TODO: use the server pattern so we can shut it down for tests..
+// TODO: tests!
+// TODO: round robin remote DNS support?  i.e. use 8.8.8.8 and 8.8.4.4?
+// TODO: failover DNS support?  i.e., 8.8.8.8 doesn't work, so use 8.8.4.4?
+// TODO: singleflight optimization?
+
 // Test with:
 // bash$ nslookup github.com. 127.0.0.1
 func main() {
 	flag.Parse()
 
+	log.Printf("Local address %v", *address)
+	log.Printf("DNS server %v", *dnsServer)
+
 	if *socks5Proxy == "" {
-		log.Fatal("-socks5 is required")
+		log.Printf("Using direct connect (no SOCKS5 proxy specified)")
+	} else {
+		log.Printf("Using SOCKS5 proxy %v", *socks5Proxy)
 	}
 
 	numWorkers := runtime.NumCPU() * 4
-	_proxyChan = make(chan proxyRequest, numWorkers)
+	jobQueue := make(chan proxyRequest, numWorkers)
 	for i := 0; i < numWorkers; i++ {
-		go proxyWorker(_proxyChan)
+		go proxyWorker(jobQueue)
 	}
 
 	// default read/write timeouts are 2s
@@ -189,7 +208,10 @@ func main() {
 		Addr: *address,
 		Net:  "tcp",
 	}
-	dns.HandleFunc(".", route)
+	dns.HandleFunc(".", func(w dns.ResponseWriter, req *dns.Msg) {
+		route(w, req, jobQueue)
+	})
+
 	go func() {
 		log.Fatal(udpServer.ListenAndServe())
 	}()
